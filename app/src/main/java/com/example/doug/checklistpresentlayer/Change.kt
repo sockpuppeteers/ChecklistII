4aa6230d7bdac9921522bf61a2db5c01ab110176
cademcniven <cademcniven@gmail.com>
2019-03-25 11:22:40 -0700

    Refactored Change

--- app/src/main/java/com/example/doug/checklistpresentlayer/Change.kt
+++ app/src/main/java/com/example/doug/checklistpresentlayer/Change.kt
@@ -1,9 +1,4 @@
 package com.example.doug.checklistpresentlayer
 
-data class Change(var taskName: String, var changedBy: User,
-                  var changeType: kAction) {
-    var changedTo: String? = null
-    constructor(_taskName: String, _changedBy: User, _changeType: kAction, _changedTo : String) : this(_taskName, _changedBy, _changeType){
-        changedTo = _changedTo
-    }
-}
+data class Change(var checklistID: Int, var userID: Int, var taskID: Int, var taskName: String, var changedBy: String,
+                  var changeType: kAction, var changedTo: String? = null)
--- app/src/main/java/com/example/doug/checklistpresentlayer/Checklist.kt
+++ app/src/main/java/com/example/doug/checklistpresentlayer/Checklist.kt
@@ -18,18 +18,20 @@ class Checklist( var name: String ) {
      *  Purpose: Logs change, the user who changed it, the type of
      *      action that was taken, and what the value was changed to
      ***************************************************************/
-    fun logChange(taskChanged: String, changedBy: User, changeType: kAction, changedTo: String) {
-        val change = Change(taskChanged, changedBy, changeType, changedTo)
+    fun logChange(taskID: Int, taskName: String, changedBy: User, changeType: kAction, changedTo: String) {
+        val change = Change(checkID, changedBy.UserID!!, taskID, taskName, changedBy.Username, changeType, changedTo)
         changes.add(change)
     }
+
     /****************************************************************
      *  Purpose: Overloaded function of logChange that does not
      *      include a "changed to" value.
      ***************************************************************/
-    fun logChange(taskChanged: String, changedBy: User, changeType: kAction) {
-        val change = Change(taskChanged, changedBy, changeType)
+    fun logChange(taskID: Int, taskName: String, changedBy: User, changeType: kAction) {
+        val change = Change(checkID, changedBy.UserID!!, taskID, taskName, changedBy.Username, changeType, null)
         changes.add(change)
     }
+
     /****************************************************************
      *  Purpose: Creates an appropriate task with a name and
      *      description, shows who made it, and adds it to a list
@@ -37,8 +39,9 @@ class Checklist( var name: String ) {
     fun createTask(name: String, createdBy: User) {
         val task = Task(name)
         tasks.add(task)
-        logChange(name, createdBy, kAction.CREATE_TASK, name)
+        logChange(task.TaskID!!, task.name, createdBy, kAction.CREATE_TASK)
     }
+
     /****************************************************************
      *  Purpose: Overloaded function of create task that includes all
      *      previous information and also includes a deadline
@@ -46,8 +49,9 @@ class Checklist( var name: String ) {
     fun createTask(name: String, deadline: String, createdBy: User) {
         val task = Task(name, deadline)
         tasks.add(task)
-        logChange(name, createdBy, kAction.CREATE_TASK, name)
+        logChange(task.TaskID!!, task.name, createdBy, kAction.CREATE_TASK)
     }
+
     /****************************************************************
      *  Purpose: Marks a task as completed and does NOT remove from
      *      the list of tasks. This is so it can be a recurring task
@@ -55,32 +59,35 @@ class Checklist( var name: String ) {
     fun completeTask(arrayIndex: Int, completedBy: User) {
 
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, completedBy, kAction.COMPLETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, completedBy, kAction.COMPLETE_TASK)
             tasks[arrayIndex].compdatetime = LocalDateTime.now().toString()
         }
     }
+
     /****************************************************************
      *  Purpose: Overloaded completeTask function that allows a user
      *      to manually enter the time that a task was completed.
      ***************************************************************/
     fun completeTask(arrayIndex: Int, completedBy: User, datetimeWhenCompleted: String) {
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, completedBy, kAction.COMPLETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, completedBy, kAction.COMPLETE_TASK)
             tasks[arrayIndex].compdatetime = datetimeWhenCompleted
         }
     }
+
     /****************************************************************
      *  Purpose: Deletes a task from the list of tasks. Deletes
      *      through an index in the table and logs who deleted what
      ***************************************************************/
     fun deleteTask(arrayIndex : Int, deletedBy: User) {
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, deletedBy, kAction.DELETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, deletedBy, kAction.DELETE_TASK)
             //tasks[arrayIndex].removeTask(uID)
             tasks.removeAt(arrayIndex)
 
         }
     }
+
     /****************************************************************
      *  Purpose: Changes the name of a task to a user-specified new
      *      value. Shows which user modified it for logs.
@@ -88,7 +95,7 @@ class Checklist( var name: String ) {
     fun changeTaskName(arrayIndex: Int, modifiedBy: User, name: String) {
 
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, modifiedBy, kAction.DELETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, modifiedBy, kAction.CHANGE_TASK_NAME, name)
             //tasks[arrayIndex].putTask(uID)
             tasks[arrayIndex].name = name
         }
@@ -100,17 +107,18 @@ class Checklist( var name: String ) {
      ***************************************************************/
     fun changeTaskDeadline(arrayIndex: Int, modifiedBy: User, deadline: String) {
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, modifiedBy, kAction.DELETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, modifiedBy, kAction.CHANGE_TASK_DEADLINE, deadline)
             tasks[arrayIndex].Deadline = deadline
         }
     }
+
     /****************************************************************
      *  Purpose: Completely removes a deadline associated with a task
      *      as long as there is a deadline associated with it.
      ***************************************************************/
     fun removeDeadline(arrayIndex: Int, modifiedBy: User) {
         if (arrayIndex >= 0 && arrayIndex < tasks.size) {
-            logChange(tasks[arrayIndex].name, modifiedBy, kAction.DELETE_TASK)
+            logChange(tasks[arrayIndex].TaskID!!, tasks[arrayIndex].name, modifiedBy, kAction.REMOVE_TASK_DEADLINE)
             tasks[arrayIndex].Deadline = ""
         }
     }
