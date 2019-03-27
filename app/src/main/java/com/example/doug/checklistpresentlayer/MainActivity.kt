b6e3bdd167ac64e0387c710e0fce3aa278e13433
cademcniven <cademcniven@gmail.com>
2019-03-27 11:04:54 -0700

    Added local storage for user data so you don't have to re-enter login data

--- app/src/main/java/com/example/doug/checklistpresentlayer/MainActivity.kt
+++ app/src/main/java/com/example/doug/checklistpresentlayer/MainActivity.kt
@@ -1,34 +1,38 @@
 package com.example.doug.checklistpresentlayer
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_login.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
-
-    var body: String? = ""
-//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//        when (item.itemId) {
-//            R.id.navigation_home -> {
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_dashboard -> {
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_notifications -> {
-//                return@OnNavigationItemSelectedListener true
-//            }
-//        }
-//        false
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         var user: UserPage
+
+        //if the USERDATA file exists already, the user doesn't
+        //have to go through the login process
+        if(userFileExists()){
+            user = getUserFromFile()
+
+            //Go to the list of lists page
+            val tempIntent = Intent(this, BaseListofLists::class.java).apply {
+                putExtra("uname", user.ViewUserName())
+                putExtra("fname", user.ViewFName())
+                putExtra("lname", user.ViewLName())
+                putExtra("UserID",user.ViewID())
+            }
+            startActivity(tempIntent)
+        }
 
         login_button.setOnClickListener {
             //establish a database connection
@@ -46,6 +50,11 @@ class MainActivity : AppCompatActivity() {
 
             //otherwise, log the user in and transfer to the list of lists page
             else {
+                //save a local file with the user's data
+                //so that they won't have to login next time
+                saveLoginLocally(user)
+
+                //Go to the list of lists page
                 val tempIntent = Intent(this, BaseListofLists::class.java).apply {
                         putExtra("uname", user.ViewUserName())
                         putExtra("fname", user.ViewFName())
@@ -60,5 +69,46 @@ class MainActivity : AppCompatActivity() {
             val tempIntent = Intent(this, Registration::class.java)
             startActivity(tempIntent)
         }
+    }
+
+    fun saveLoginLocally(user: UserPage){
+        //convert user to a JSON string
+        val gson = Gson()
+        val userJson = gson.toJson(user)
+
+        //context will give us access to our local files directory
+        var context = applicationContext
+
+        val filename = "USERDATA"
+        val directory = context.filesDir
+
+        //write the file USERDATA to local directory
+        val file = File(directory, filename)
+        FileOutputStream(file).use {
+            it.write(userJson.toByteArray())
+        }
+
+        //val inputAsString = FileInputStream(file).bufferedReader().use { it.readText() }
+        //println(inputAsString)
+    }
+
+    fun userFileExists() : Boolean {
+        return File(applicationContext.filesDir, "USERDATA").exists()
+    }
+
+    fun getUserFromFile() : UserPage {
+        //context will give us access to our local files directory
+        var context = applicationContext
+
+        val filename = "USERDATA"
+        val directory = context.filesDir
+
+        //read from USERDATA and store it as a string
+        val file = File(directory, filename)
+        val fileData = FileInputStream(file).bufferedReader().use { it.readText() }
+
+        //create a UserPage object based on the JSON from the file
+        val gson = Gson()
+        return gson.fromJson(fileData, UserPage::class.java)
     }
 }
