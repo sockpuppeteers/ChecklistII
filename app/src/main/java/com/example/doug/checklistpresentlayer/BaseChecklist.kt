a85cd68d411041c2fdbc87736aea05c41faace19
RogueLament <45111295+RogueLament@users.noreply.github.com>
2019-05-08 13:21:39 -0700

    fixed mewrge stupidity

--- app/src/main/java/com/example/doug/checklistpresentlayer/BaseChecklist.kt
+++ app/src/main/java/com/example/doug/checklistpresentlayer/BaseChecklist.kt
@@ -1276,20 +1276,18 @@ class BaseChecklist : AppCompatActivity(){
                             db.AddUserToList(u.UserID as Int, currentChecklist.listID as Int)
                             rightsubMenu.add(0, Menu.FIRST + 7, Menu.FIRST, u.Username)
                         }
+                        searchItem.collapseActionView()
+                        val activity: Activity = this@BaseChecklist
+                        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                        //Find the currently focused view, so we can grab the correct window token from it.
+                        var view = activity.currentFocus
+                        //If no view currently has focus, create a new one, just so we can grab a window token from it
+                        if (view == null) {
+                            view = View(activity)
+                        }
+                        imm.hideSoftInputFromWindow(view.windowToken, 0)
                         return true
                     }
-                    searchItem.collapseActionView()
-                    val activity : Activity = this@BaseChecklist
-                    val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                    //Find the currently focused view, so we can grab the correct window token from it.
-                    var view = activity.currentFocus;
-                    //If no view currently has focus, create a new one, just so we can grab a window token from it
-                    if (view == null) {
-                        view = View(activity)
-                    }
-                    imm.hideSoftInputFromWindow(view.windowToken, 0)
-                    return true
-                }
 
                     override fun onQueryTextChange(newText: String?): Boolean {
                         return true
